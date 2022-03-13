49b58bbe317fcc89e772a8a393901da6db7ddd21
rmd3 <michaeldanjolell@gmail.com>
2022-03-13 18:15:40 -0400

    Add project files.

---
+++ D'Anjolell_exam1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace D_Anjolell_exam1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCh = new System.Windows.Forms.TextBox();
+            this.lblEntry = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnEcrypt = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblChResult = new System.Windows.Forms.Label();
+            this.lblEncryptResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCh
+            // 
+            this.txtCh.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtCh.Location = new System.Drawing.Point(411, 12);
+            this.txtCh.Name = "txtCh";
+            this.txtCh.Size = new System.Drawing.Size(100, 38);
+            this.txtCh.TabIndex = 0;
+            // 
+            // lblEntry
+            // 
+            this.lblEntry.AutoSize = true;
+            this.lblEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblEntry.Location = new System.Drawing.Point(150, 19);
+            this.lblEntry.Name = "lblEntry";
+            this.lblEntry.Size = new System.Drawing.Size(230, 31);
+            this.lblEntry.TabIndex = 1;
+            this.lblEntry.Text = "Enter a character:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnSubmit.Location = new System.Drawing.Point(253, 84);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(127, 51);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnEcrypt
+            // 
+            this.btnEcrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnEcrypt.Location = new System.Drawing.Point(281, 228);
+            this.btnEcrypt.Name = "btnEcrypt";
+            this.btnEcrypt.Size = new System.Drawing.Size(210, 51);
+            this.btnEcrypt.TabIndex = 5;
+            this.btnEcrypt.Text = "Ecryption";
+            this.btnEcrypt.UseVisualStyleBackColor = true;
+            this.btnEcrypt.Click += new System.EventHandler(this.btnEcrypt_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnExit.Location = new System.Drawing.Point(411, 84);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 51);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblChResult
+            // 
+            this.lblChResult.AutoSize = true;
+            this.lblChResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblChResult.Location = new System.Drawing.Point(182, 161);
+            this.lblChResult.Name = "lblChResult";
+            this.lblChResult.Size = new System.Drawing.Size(0, 31);
+            this.lblChResult.TabIndex = 7;
+            // 
+            // lblEncryptResult
+            // 
+            this.lblEncryptResult.AutoSize = true;
+            this.lblEncryptResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblEncryptResult.Location = new System.Drawing.Point(109, 305);
+            this.lblEncryptResult.Name = "lblEncryptResult";
+            this.lblEncryptResult.Size = new System.Drawing.Size(0, 31);
+            this.lblEncryptResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 782);
+            this.Controls.Add(this.lblEncryptResult);
+            this.Controls.Add(this.lblChResult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEcrypt);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblEntry);
+            this.Controls.Add(this.txtCh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCh;
+        private System.Windows.Forms.Label lblEntry;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnEcrypt;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblChResult;
+        private System.Windows.Forms.Label lblEncryptResult;
+    }
+}
+
---
+++ D'Anjolell_exam1/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Anjolell_exam1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the program when clicked
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Richard D'Anjolell
+            //Try catch statement if there is an error accessing the file
+            try
+            {
+                //Declare variables
+                int occuranceNumber = 0;
+                const int ARRAY_SIZE = 16;
+                string[] fileLines = new string[ARRAY_SIZE];
+                char[] charLines = new char[62];
+                List<char> tempList;
+                List<char> charList;
+                string userInput = txtCh.Text;
+                StreamReader inputFile;
+
+                //Open the file
+                inputFile = File.OpenText("..\\..\\hhlyrics.txt");
+
+                //Declare the index for the arrays
+                int indexNum = 0;
+
+                //Sends lines from the file into an array
+                while (indexNum < fileLines.Length && !inputFile.EndOfStream)
+                {
+                    //Set array number to line in the file
+                    fileLines[indexNum] = inputFile.ReadLine();
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Has a list of characters of the first line in the array
+                charList = fileLines[0].ToCharArray().ToList();
+
+                //Resets index to one for next while loop since 0 has already been done
+                indexNum = 1;
+
+                //While loop to have all characters in charList
+                while (indexNum < fileLines.Length)
+                {
+                    //Find all characters, send to temp variable to be added to final list
+                    charLines = fileLines[indexNum].ToCharArray();
+                    tempList = charLines.ToList();
+                    charList.AddRange(tempList);
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Resets index for next while loop
+                indexNum = 0;
+
+                //While loop to find all the times the user character occurs
+                while (indexNum < charList.Count())
+                {
+                    //If statement when the character from the list equals the user's
+                    if (charList[indexNum] == userInput[0])
+                    {
+                        //Increment the number of times the character appears
+                        occuranceNumber++;
+                    }
+
+                //Increment index counter
+                indexNum++;
+                }
+
+                //Outputs the results found from the program
+                lblChResult.Text = "This character shows up " + occuranceNumber.ToString() + " times.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEcrypt_Click(object sender, EventArgs e)
+        {
+            //Try catch statement if there is an error accessing the file
+            try
+            {
+                //Declare variables
+                const int ARRAY_SIZE = 16;
+                string[] fileLines = new string[ARRAY_SIZE];
+                char[] charLines = new char[62];
+                List<char> tempList;
+                List<char> charList;
+                var finalTranscript = new StringBuilder();
+                StreamReader inputFile;
+
+                //Open the file
+                inputFile = File.OpenText("..\\..\\hhlyrics.txt");
+
+                //Declare the index for the arrays
+                int indexNum = 0;
+
+                //Sends lines from the file into an array
+                while (indexNum < fileLines.Length && !inputFile.EndOfStream)
+                {
+                    //Set array number to line in the file
+                    fileLines[indexNum] = inputFile.ReadLine();
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Has a list of characters of the first line in the array
+                charList = fileLines[0].ToCharArray().ToList();
+
+                //Resets index to one for next while loop since 0 has already been done
+                indexNum = 1;
+
+                //While loop to have all characters in charList
+                while (indexNum < fileLines.Length)
+                {
+                    //Find all characters, send to temp variable to be added to final list
+                    charLines = fileLines[indexNum].ToCharArray();
+                    tempList = charLines.ToList();
+                    charList.AddRange(tempList);
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Resets index for next while loop
+                indexNum = 0;
+
+                //While loop to encrypt certain characters
+                while (indexNum < charList.Count())
+                {
+                    //A set of if statements to encrypt certain characters
+                    if (charList[indexNum] == "A".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "Z".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "a".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "z".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "D".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "W".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "d".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "w".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "H".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "T".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "h".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "t".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "K".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "R".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "k".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "r".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "M".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "P".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "m".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "p".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "O".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "N".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "o".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "n".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "S".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "J".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "s".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "j".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "X".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "E".ToCharArray()[0];
+                    }
+                    else if (charList[indexNum] == "x".ToCharArray()[0])
+                    {
+                        charList[indexNum] = "e".ToCharArray()[0];
+                    }
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Resets index for next while loop
+                indexNum = 0;
+
+                //Combine all the characters into one cohesive stringbuilder to be outputed
+                while (indexNum < charList.Count())
+                {
+                    finalTranscript.Append(charList[indexNum]);
+
+                    //If statement to add a newline, so the stringbuilder fits on the form
+                    if (finalTranscript.Length % 45 == 0)
+                    {
+                        finalTranscript.Append("\n");
+                    }
+
+                    //Increment index counter
+                    indexNum++;
+                }
+                
+                //Outputs stringbuilder that was made in the while loop
+                lblEncryptResult.Text = finalTranscript.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
